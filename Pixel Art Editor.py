1e3b48f426670f0ca121a9946e2abc8d3c463faf
Sebastián Solís <s.solis.3@estudiantec.cr>
2024-06-04 22:03:59 -0600

    Clase Matriz unificada

--- Pixel Art Editor.py
+++ Pixel Art Editor.py
@@ -32,11 +32,23 @@ class ASCII:
     def ObtenerSimbolo(self):
         return self.simbolo
 
+class Boton:
+    def __init__(self, label, callback, user_data=None, width=100, height=30):
+        self.label = label
+        self.callback = callback
+        self.user_data = user_data
+        self.width = width
+        self.height = height
+
+    def CrearBoton(self):
+        dpg.add_button(label=self.label, callback=self.callback, user_data=self.user_data, width=self.width, height=self.height)
+        
 class Matriz:
     def __init__(self, width, height, default_value=0):
         self.width = width
         self.height = height
         self.grid = [[default_value for _ in range(width)] for _ in range(height)]
+        self.cell_size = CELL_SIZE
 
     @classmethod
     def LeerTXT(cls, filepath, width=WIDTH, height=HEIGHT, default_value=0):
@@ -52,18 +64,123 @@ class Matriz:
 
     def ConvertirAGrid(self, colores_dict):
         return [[colores_dict.get(val, colores_dict[0]).ObtenerRGB() for val in fila] for fila in self.grid]
-    
-class Boton:
-    def __init__(self, label, callback, user_data=None, width=100, height=30):
-        self.label = label
-        self.callback = callback
-        self.user_data = user_data
-        self.width = width
-        self.height = height
 
-    def CrearBoton(self):
-        dpg.add_button(label=self.label, callback=self.callback, user_data=self.user_data, width=self.width, height=self.height)
+    def DibujaGrid(self):
+        dpg.delete_item("Dibujo", children_only=True)
+        for y in range(self.height):
+            for x in range(self.width):
+                dpg.draw_rectangle([x * self.cell_size, y * self.cell_size],
+                                   [(x + 1) * self.cell_size, (y + 1) * self.cell_size],
+                                   color=(0, 0, 0, 255), fill=self.grid[y][x], parent="Dibujo")
+        for x in range(self.width + 1):
+            dpg.draw_line([x * self.cell_size, 0], [x * self.cell_size, self.height * self.cell_size], color=(0, 0, 0, 255), thickness=1, parent="Dibujo")
+        for y in range(self.height + 1):
+            dpg.draw_line([0, y * self.cell_size], [self.width * self.cell_size, y * self.cell_size], color=(0, 0, 0, 255), thickness=1, parent="Dibujo")
 
+    def MouseDragHandler(self,sender, app_data):
+        self.ClickPosicion(sender, app_data, None)
+        
+    def ClickPosicion(self, sender, app_data, user_data):
+        mouse_pos = dpg.get_mouse_pos()
+        x = int(mouse_pos[0] // self.cell_size)
+        y = int(mouse_pos[1] // self.cell_size)
+        if 0 <= x < self.width and 0 <= y < self.height:
+            self.grid[y][x] = ColorActual.ObtenerRGB()
+            self.DibujaGrid()
+
+    def ValoresColorANumero(self, rgb): 
+        for num, color in ValoresColores.items():
+            if color.ObtenerRGB() == rgb:
+                return num
+        return 0
+
+    def DibujaMatriz(self):
+        matriz = [[self.ValoresColorANumero(self.grid[y][x]) for x in range(self.width)] for y in range(self.height)]
+        for fila in matriz:
+            print(" ".join(map(str, fila)))
+
+    def DibujaASCII(self):
+        matriz = [[self.ValoresColorANumero(self.grid[y][x]) for x in range(self.width)] for y in range(self.height)]
+        for fila in matriz:
+            print("".join(DiccionarioASCII[val] for val in fila))
+
+    def ZoomIn(self):
+        if self.cell_size < MAX_CELL_SIZE:
+            self.cell_size += 1
+            self.DibujaGrid()
+
+    def ZoomOut(self):
+        if self.cell_size > MIN_CELL_SIZE:
+            self.cell_size -= 1
+            self.DibujaGrid()
+
+    def GuardaImagen(self):
+        grid_expandido = []
+        for fila in self.grid:
+            nueva_fila = []
+            for color in fila:
+                nueva_fila.extend([color[:3], color[:3]])
+            grid_expandido.extend([nueva_fila, nueva_fila])
+        
+        pixel_data = np.array(grid_expandido, dtype=np.uint8)
+        imagen = Image.fromarray(pixel_data)
+        folder = os.path.dirname(os.path.abspath(__file__))
+        directorio_path = os.path.join(folder, "PixelArt.png")
+        txt_path = os.path.join(folder, "PixelArt.txt")
+        
+        imagen.save(directorio_path)
+        print(f"Imagen guardada en el directorio: {directorio_path}")
+
+        matriz = [[self.ValoresColorANumero(self.grid[y][x]) for x in range(self.width)] for y in range(self.height)]
+        with open(txt_path, 'w') as file:
+            for fila in matriz:
+                file.write(" ".join(map(str, fila)) + "\n")
+        print(f"Matriz guardada en el directorio: {txt_path}")
+
+    def Altocontraste(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                valor = self.ValoresColorANumero(self.grid[y][x])
+                if 0 <= valor <= 4:
+                    self.grid[y][x] = ValoresColores[1].ObtenerRGB()
+                elif 5 <= valor <= 9:
+                    self.grid[y][x] = ValoresColores[9].ObtenerRGB()
+        self.DibujaGrid()
+
+    def Negativo(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                valor = self.ValoresColorANumero(self.grid[y][x])
+                self.grid[y][x] = ValoresColores[9 - valor].ObtenerRGB()
+        self.DibujaGrid()
+
+    def Rotarderecha(self):
+        filas = len(self.grid)
+        columnas = len(self.grid[0])
+        nueva_grid = [[None] * filas for _ in range(columnas)]
+        for i in range(filas):
+            for j in range(columnas):
+                nueva_grid[j][filas - 1 - i] = self.grid[i][j]
+        self.grid = nueva_grid
+        self.DibujaGrid()
+
+    def Rotarizquierda(self):
+        filas = len(self.grid)
+        columnas = len(self.grid[0])
+        nueva_grid = [[None] * filas for _ in range(columnas)]
+        for i in range(filas):
+            for j in range(columnas):
+                nueva_grid[filas - 1 - j][i] = self.grid[i][j]
+        self.grid = nueva_grid
+        self.DibujaGrid()
+
+    def InvertirVertical(self):
+        self.grid = self.grid[::-1]
+        self.DibujaGrid()
+
+    def InvertirHorizontal(self):
+        self.grid = [fila[::-1] for fila in self.grid]
+        self.DibujaGrid()
 
 # Instanciación de colores
 Borrador = Colores("Borrador", 255, 255, 255, 255, 0)
@@ -130,175 +247,20 @@ DiccionarioASCII = {
 
 # Valores iniciales
 ColorActual = Colores["Negro"]
+
+# Inicialización de la matriz
 grid = [[Colores["Borrador"].ObtenerRGB() for _ in range(WIDTH)] for _ in range(HEIGHT)]
+matriz = Matriz(WIDTH, HEIGHT, default_value=Colores["Borrador"].ObtenerRGB())
+matriz.grid = grid
 
-def DibujaGrid(): 
-    dpg.delete_item("Dibujo", children_only=True) #el children_only elimina solo las "subventanas", no la principal
-    for y in range(HEIGHT):
-        for x in range(WIDTH): #For anidados para dibujar la matriz
-            dpg.draw_rectangle([x * CELL_SIZE, y * CELL_SIZE],
-                               [(x + 1) * CELL_SIZE, (y + 1) * CELL_SIZE],
-                               color=(0, 0, 0, 255), fill=grid[y][x], parent="Dibujo") #Dibuja la matriz, utiliza el CELL_SIZE para saber de que tamano representar cada pixel
-    for x in range(WIDTH + 1):
-        dpg.draw_line([x * CELL_SIZE, 0], [x * CELL_SIZE, HEIGHT * CELL_SIZE], color=(0, 0, 0, 255), thickness=1, parent="Dibujo")
-    for y in range(HEIGHT + 1):
-        dpg.draw_line([0, y * CELL_SIZE], [WIDTH * CELL_SIZE, y * CELL_SIZE], color=(0, 0, 0, 255), thickness=1, parent="Dibujo")
-
-def ClickPosicion(sender, app_data, user_data): #Funcion para detectar donde se presiona, aunque tiene errores de colision, es temporal
-    mouse_pos = dpg.get_mouse_pos() #mouse_pos es una funcion de DearPyGUI
-    x = int(mouse_pos[0] // CELL_SIZE)
-    y = int(mouse_pos[1] // CELL_SIZE)
-    if 0 <= x < WIDTH and 0 <= y < HEIGHT: #Verifica si el mouse clickea dentro del grid
-        grid[y][x] = ColorActual.ObtenerRGB()   #se le suma un 2 a Y, y se le resta 1 a X para mejorar la precision a la hora de dibujar
-        DibujaGrid()
-
-def CambioAColorElegido(sender, app_data, user_data): #Funcion que cambia el color a colocar segun seleccion del usuario
+def CambioAColorElegido(sender, app_data, user_data):
     global ColorActual
     ColorActual = Colores[user_data]
 
-def DibujaMatriz(): #Dibuja la matriz en consola (de momento), segun los pixeles que se encuentre y su numero asociado
-    global Matriz
-    Matriz = [[ValoresColorANumero(grid[y][x]) for x in range(WIDTH)] for y in range(HEIGHT)]
-    for fila in Matriz:
-        print(" ".join(map(str, fila)))
-
-def DibujaASCII(): #Lo mismo, pero con ASCII
-    Matriz = [[ValoresColorANumero(grid[y][x]) for x in range(WIDTH)] for y in range(HEIGHT)]
-    for fila in Matriz:
-        print("".join(DiccionarioASCII[val] for val in fila))
-
-def ValoresColorANumero(rgb): 
-    for num, color in ValoresColores.items():
-        if color.ObtenerRGB() == rgb:
-            return num
-    return 0  # Retorna a borrador (BLanco) si no se encuentra
-
-def ZoomIn(): #Aumenta el tamano de la celda en 1 unidad (Tamano maximo 20)
-    global CELL_SIZE
-    if CELL_SIZE < MAX_CELL_SIZE:
-        CELL_SIZE += 1
-        DibujaGrid()
-
-def ZoomOut(): #Decrementa el tamano de la celda en 1 unidad (Tamano minimo 5)
-    global CELL_SIZE
-    if CELL_SIZE > MIN_CELL_SIZE:
-        CELL_SIZE -= 1
-        DibujaGrid()
-
-def GuardaImagen():
-    GridExpandido = [] # Crea un nuevo grid donde cada pixel se convierte en un 2x2 (Como el checkerboard de la playstation)
-    for fila in grid:
-        NuevaFila = []
-        for color in fila:
-            NuevaFila.extend([color[:3], color[:3]])  # Duplica cada pixel horizontalmente
-        GridExpandido.extend([NuevaFila, NuevaFila])  # Verticalmente
-    
-    pixel_data = np.array(GridExpandido, dtype=np.uint8) #Convierte el grid expandido en un array de numpy 
-    Imagen = Image.fromarray(pixel_data)
-    Folder = os.path.dirname(os.path.abspath(__file__)) 
-    DirectorioPath = os.path.join(Folder, "PixelArt.png")
-    TXTPath = os.path.join(Folder, "PixelArt.txt")
-    
-    Imagen.save(DirectorioPath)
-    print(f"Imagen guardada en el directorio: {DirectorioPath}")
-
-    Matriz = [[ValoresColorANumero(grid[y][x]) for x in range(WIDTH)] for y in range(HEIGHT)]
-    with open(TXTPath, 'w') as file:
-        for fila in Matriz:
-            file.write(" ".join(map(str, fila)) + "\n")
-    print(f"Matriz guardada en el directorio: {TXTPath}")
-    
-def ImportarImagen(sender, app_data, user_data):
-    DirectorioPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'matriz.txt')
-    if os.path.exists(DirectorioPath):
-        MatrizImportada = Matriz.LeerTXT(DirectorioPath)
-        global grid
-        grid = MatrizImportada.ConvertirAGrid(ValoresColores)
-        DibujaGrid()
-    else:
-        print(f"No se encontró el archivo {DirectorioPath}")
-
-def BorraImagen(): #Convierte todos los valores del grid en 0 (Blanco)
-    global grid
-    grid = [[Colores["Borrador"].ObtenerRGB() for _ in range(WIDTH)] for _ in range(HEIGHT)]
-    DibujaGrid()
-
-def Altocontraste():
-    global grid
-    for y in range(HEIGHT):
-        for x in range(WIDTH):
-            valor = ValoresColorANumero(grid[y][x])
-            if 0 <= valor <= 4:
-                grid[y][x] = ValoresColores[1].ObtenerRGB()  # Cambiar a color Plata
-            elif 5 <= valor <= 9:
-                grid[y][x] = ValoresColores[9].ObtenerRGB()  # Cambiar a color Negro
-    DibujaGrid()
-
-def Negativo():
-    global grid
-    for y in range(HEIGHT):
-        for x in range(WIDTH):
-            valor = ValoresColorANumero(grid[y][x])
-            if valor == 9:
-                grid[y][x] = ValoresColores[0].ObtenerRGB()
-            elif valor == 8:
-                grid[y][x] = ValoresColores[1].ObtenerRGB()
-            elif valor == 7:
-                grid[y][x] = ValoresColores[2].ObtenerRGB()
-            elif valor == 6:
-                grid[y][x] = ValoresColores[3].ObtenerRGB()
-            elif valor == 5:
-                grid[y][x] = ValoresColores[4].ObtenerRGB()
-            elif valor == 4:
-                grid[y][x] = ValoresColores[5].ObtenerRGB()
-            elif valor == 3:
-                grid[y][x] = ValoresColores[6].ObtenerRGB()
-            elif valor == 2:
-                grid[y][x] = ValoresColores[7].ObtenerRGB()
-            elif valor == 1:
-                grid[y][x] = ValoresColores[8].ObtenerRGB()
-            elif valor == 0:
-                grid[y][x] = ValoresColores[9].ObtenerRGB()
-
-    DibujaGrid()
-    
-def Rotarderecha():
-    global grid
-    filas = len(grid)
-    columnas = len(grid[0])
-    nueva_grid = [[None] * filas for _ in range(columnas)]
-    for i in range(filas):
-        for j in range(columnas):
-            nueva_grid[j][filas - 1 - i] = grid[i][j]
-    filas, columnas = columnas, filas
-    grid = nueva_grid
-    DibujaGrid()
-
-def Rotarizquierda():
-    global grid
-    filas = len(grid)
-    columnas = len(grid[0])
-    nueva_grid = [[None] * filas for _ in range(columnas)]
-    for i in range(filas):
-        for j in range(columnas):
-            nueva_grid[filas - 1 - j][i] = grid[i][j]
-    filas, columnas = columnas, filas
-    grid = nueva_grid
-    DibujaGrid()
-
-def InvertirVertical():
-    global grid
-    grid = grid[::-1]
-    DibujaGrid()
-
-def InvertirHorizontal():
-    global grid
-    grid = [fila[::-1] for fila in grid]
-    DibujaGrid()
-
+# Dibuja la interfaz
 dpg.create_context()
 
-with dpg.font_registry(): #Fuente para el editor 
+with dpg.font_registry():
     FuentePorDefecto = dpg.add_font("Roboto-Light.ttf", 20)
     FuenteSecundaria = dpg.add_font("Roboto-Light.ttf", 10)
 
@@ -306,29 +268,28 @@ with dpg.window(label="Pixel Art Editor", tag="Primary Window"):
     with dpg.group(horizontal=True):
         with dpg.group():
             with dpg.drawlist(width=WIDTH * CELL_SIZE, height=HEIGHT * CELL_SIZE, tag="Dibujo"):
-                dpg.set_item_callback("Dibujo", ClickPosicion)
+                dpg.set_item_callback("Dibujo", matriz.ClickPosicion)
 
         with dpg.group():
-            # Crea botones para los colores
             for NombreDeColor in Colores:
                 Boton(label=NombreDeColor, callback=CambioAColorElegido, user_data=NombreDeColor).CrearBoton()
-            
-            Boton(label="Zoom In", callback=ZoomIn).CrearBoton()
-            Boton(label="Zoom Out", callback=ZoomOut).CrearBoton()
-            Boton(label="Mostrar Matriz", callback=DibujaMatriz, width=140).CrearBoton()
-            Boton(label="Mostrar ASCII", callback=DibujaASCII, width=140).CrearBoton()
-            Boton(label="Importar imagen", callback=ImportarImagen, width=140).CrearBoton()
-            Boton(label="Guardar Imagen", callback=GuardaImagen, width=140).CrearBoton()
-            Boton(label="Alto contraste", callback=Altocontraste, width=140).CrearBoton()
-            Boton(label="Negativo", callback=Negativo, width=140).CrearBoton()
-            Boton(label="X", callback=BorraImagen).CrearBoton()
-            Boton(label="Rotar a la derecha", callback=Rotarderecha, width=140).CrearBoton()
-            Boton(label="Rotar a la izquierda", callback=Rotarizquierda, width=140).CrearBoton()
-            Boton(label="Invertir vertical", callback=InvertirVertical, width=140).CrearBoton()
-            Boton(label="Invertir horizontal", callback=InvertirHorizontal, width=140).CrearBoton()
+
+            Boton(label="Zoom In", callback=matriz.ZoomIn).CrearBoton()
+            Boton(label="Zoom Out", callback=matriz.ZoomOut).CrearBoton()
+            Boton(label="Mostrar Matriz", callback=matriz.DibujaMatriz, width=140).CrearBoton()
+            Boton(label="Mostrar ASCII", callback=matriz.DibujaASCII, width=140).CrearBoton()
+            Boton(label="Guardar Imagen", callback=matriz.GuardaImagen, width=140).CrearBoton()
+            Boton(label="Alto contraste", callback=matriz.Altocontraste, width=140).CrearBoton()
+            Boton(label="Negativo", callback=matriz.Negativo, width=140).CrearBoton()
+            Boton(label="Rotar a la derecha", callback=matriz.Rotarderecha, width=140).CrearBoton()
+            Boton(label="Rotar a la izquierda", callback=matriz.Rotarizquierda, width=140).CrearBoton()
+            Boton(label="Invertir vertical", callback=matriz.InvertirVertical, width=140).CrearBoton()
+            Boton(label="Invertir horizontal", callback=matriz.InvertirHorizontal, width=140).CrearBoton()
             dpg.bind_font(FuentePorDefecto)
-            
-DibujaGrid()
+
+matriz.DibujaGrid()
+with dpg.handler_registry():
+    dpg.add_mouse_drag_handler(callback=matriz.MouseDragHandler) #función de dpg que captura el click del mouse
 
 dpg.create_viewport(title='Pixel Art Editor', width=800, height=800)
 dpg.setup_dearpygui()
@@ -336,4 +297,3 @@ dpg.show_viewport()
 dpg.set_primary_window("Primary Window", True)
 dpg.start_dearpygui()
 dpg.destroy_context()
-
